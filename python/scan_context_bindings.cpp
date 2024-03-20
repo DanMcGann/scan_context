0ad2b3c722ee971db7921af8b7fa94d59c146540
Dan McGann <dmcgann@andrew.cmu.edu>
2024-03-20 10:47:48 -0400

    Add Database bindings

--- python/scan_context_bindings.cpp
+++ python/scan_context_bindings.cpp
@@ -4,6 +4,7 @@
 
 #include <Eigen/Dense>
 
+#include "scan_context/database.h"
 #include "scan_context/scan_context.h"
 
 // Use short form as it appears alot
@@ -11,6 +12,7 @@ namespace py = pybind11;
 
 // Pybind will automatically convert between Eigen <-> Numpy to support numpy format points use use the following type
 using ScanContextPybind = ScanContext<Eigen::VectorXd>;
+using ScanContextDatabasePybind = ScanContextDatabase<Eigen::VectorXd>;
 
 PYBIND11_MODULE(scan_context_python, m) {
   py::module gtsam = py::module::import("numpy");
@@ -31,4 +33,18 @@ PYBIND11_MODULE(scan_context_python, m) {
       .def_readwrite("number_rings", &ScanContextPybind::Params::number_rings)
       .def_readwrite("max_range", &ScanContextPybind::Params::max_range)
       .def("equals", &ScanContextPybind::Params::equals, py::arg("other"));
+
+  py::class_<ScanContextDatabasePybind>(m, "ScanContextDatabase")
+      .def(py::init<const ScanContextDatabasePybind::Params &>(), py::arg("params"))
+      .def("insert", &ScanContextDatabasePybind::insert, py::arg("key"), py::arg("scan_context"))
+      .def("remove", &ScanContextDatabasePybind::remove, py::arg("key"))
+      .def("query", &ScanContextDatabasePybind::query, py::arg("query"))
+      .def("queryK", &ScanContextDatabasePybind::queryK, py::arg("query"), py::arg("k"));
+
+  py::class_<ScanContextDatabasePybind::Params>(m, "ScanContextDatabaseParams")
+      .def(py::init<>())
+      .def_readwrite("sc_params", &ScanContextDatabasePybind::Params::sc_params)
+      .def_readwrite("number_ring_key_nn", &ScanContextDatabasePybind::Params::number_ring_key_nn)
+      .def_readwrite("query_distance_threshold", &ScanContextDatabasePybind::Params::query_distance_threshold)
+      .def_readwrite("kdtree_rebuild_threshold", &ScanContextDatabasePybind::Params::kdtree_rebuild_threshold);
 }
